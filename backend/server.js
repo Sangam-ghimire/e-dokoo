2f628c97d77306bd315915933ebd802d6edc71cf
Sangam Ghimire <1sangamghimire1@gmail.com>
2022-09-15 22:21:16 +0545

    Node server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
-const app = require('/app');
+const app = require("./app");
 
-const dotenv =require('dotenv');
+const dotenv = require("dotenv");
 
 //setting up config file
 
-dotenv.config({path:'backend/config/config.env'})
+dotenv.config({ path: "backend/config/config.env" });
 
-app.listen(process.env.Port,()=>{
-    console.log('Server started on PORT: ${process.env.PORT}')
-})
+app.listen(process.env.Port, () => {
+  const port = process.env.port;
+  const mode = process.env.NODE_ENV;
+  console.log("Server started on PORT",process.env.port);
+  console.log("Operating on mode:",process.env.NODE_ENV);
+});
